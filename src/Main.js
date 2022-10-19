ba07f1408af2054483cd4e0757c954726586acb6
TheJangus <jangus@umich.edu>
2022-10-19 00:08:52 +0000

    first and only - whoops

---
+++ src/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import HornedBeasts from './HornedBeasts';
+
+class Main extends React.Component{
+    render(){
+        return(
+            <>
+            <Main>
+            <>
+            <HB
+                animalName="Horned Frog"
+                imageUrl="https://upload.wikimedia.org/wikipedia/commons/5/5b/Suriname_Horned_Frog_%28Ceratophrys_cornuta%29_%287774154124%29.jpg"
+                description="Suriname Horned Frog not amused"
+            />
+             <HB
+                animalName="Charlie the Unicron"
+                imageUrl="https://static.wikia.nocookie.net/filmcow/images/d/de/Charlie_episode_4.png/revision/latest?cb=20170628143744"
+                description="Charlie on his way to Candy Mountain"
+            />
+            </>
+            </Main>
+            </>
+        )
+    }
+}
+
+export default Main;
