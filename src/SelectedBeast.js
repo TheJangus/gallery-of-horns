bc032d35834cffc70ed37225d101701f6d9b382c
TheJangus <jangus@umich.edu>
2022-10-20 00:05:14 +0000

    i am horrible at making commits and need to get better but I get too in the zone

---
+++ src/SelectedBeast.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+
+class SelectedBeast extends React.Component {
+    render() {
+        const displayModal = this.props.displayModal;
+        const hideModal = this.props.hideModal;
+        const image = this.props.selectedBeast.image_url;
+        const title = this.props.selectedBeast.title;
+        const description = this.props.selectedBeast.description;
+        const keyword = this.props.selectedBeast.keyword;
+
+        return (
+            <Modal show={displayModal} onHide={this.props.hideModal}>
+                <Modal.Dialog>
+                    <Modal.Header>
+                        <h2>Selected Horned Good Guys</h2>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Card style={{ width: '26rem' }}>
+                            <Card.Img src={image} />
+                            <Card.Body>
+                                <Card.Title>{title}</Card.Title>
+                                <Card.Text>{description}</Card.Text>
+                                <Card.Text>{keyword}</Card.Text>
+                                <Button onClick={hideModal} variant="primary" size="lg" block>CLOSE</Button>
+                            </Card.Body>
+                        </Card>
+                    </Modal.Body>
+                </Modal.Dialog>
+            </Modal>
+        )
+    }
+}
+
+export default SelectedBeast;
